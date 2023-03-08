107d2ebe77c3ec9c3ed8ae45bea0af06018c3f41
mosafari <m.safari2680@gmail.com>
2023-03-08 21:48:54 +0330

    adding app level urls and create home page view

--- DjangoBloodTest/BloodTest/views.py
+++ DjangoBloodTest/BloodTest/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    if request.method == 'GET':
+        return HttpResponse("home", status=200)
